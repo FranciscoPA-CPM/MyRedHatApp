f2660435073d4765f5c6144c446cbe551d793d10
Francisco Padilla <francisco_padilla@cpm.coop>
2026-03-12 13:00:21 -0600

    version nueva 7
    ahora si es la definitva

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
-app.MapGet("/", () => Results.Json("hello webhook v6 Francisco este mensaje es el más nuevo"));
+app.MapGet("/", () => Results.Json("hello webhook v7 Francisco este mensaje es el más nuevo y definitiva"));
 
 
 // var message = Environment.GetEnvironmentVariable("MESSAGE") ?? "default";
